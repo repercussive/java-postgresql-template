0198e5d4bde0c8852b194c2ca569e2ee6c427c20
Liam Robertson <liam2robertson@gmail.com>
2023-09-15 00:21:18 +0100

    Add hello world

--- java_project/src/Program.java
+++ java_project/src/Program.java
@@ -3,6 +3,6 @@ public class Program {
     private static String connectionString = "jdbc:postgresql:sample?user=postgres&password=password";
 
     public static void main(String[] args) {
-       
+       System.out.println("Hello, world!");
     }
 }
